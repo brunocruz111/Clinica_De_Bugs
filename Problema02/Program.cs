965722189a3174ed10922c53aff40c2e8c1043c8
Alan Lino dos Reis <alaanlino@gmail.com>
2025-09-02 20:50:23 -0300

    Resolucao
    Resolucao dos exercicios 1 a 3

--- Problema02/Program.cs
+++ Problema02/Program.cs
@@ -1,10 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-float notaProva = 8.5;
-int quantidadeAlunos = "42";
-string nomeCurso = 'Fundamentos de C#'; bool pagamentoEfetuado = "false";
-char digitoVerificador = "7"
+
+//Nota prova representada como double.
+double notaProva = 8.5;
+
+//Atribuicao de uma variavel inteira nao pode estar entre aspas.
+int quantidadeAlunos = 42;
+
+//Variavel string na atribuicao tem que ser feito entre aspas duplas.
+string nomeCurso = "Fundamentos de C#";
+
+//Variavel boolean recebe os parametros true ou false sem aspas.
+bool pagamentoEfetuado = false;
+
+//Variavel char recebe atribuicao entre aspas simples e esta faltando ponto e virgula no final da linha.
+char digitoVerificador = '7';
+
 Console.WriteLine(notaProva);
 Console.WriteLine(quantidadeAlunos);
-Console.WriteLine(nomecurso);
+//Variavel nomeCurso esta escrita de forma incorreta.
+Console.WriteLine(nomeCurso);
 Console.WriteLine(pagamentoEfetuado);
 Console.WriteLine(digitoVerificador);
