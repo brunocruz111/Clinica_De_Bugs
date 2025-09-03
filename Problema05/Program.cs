7781a70df749c6b2326e6a9f4ff4875695fc57a8
NetoDog <hilariocancineto@gmail.com>
2025-09-02 21:17:22 -0300

    Update Program.cs

--- Problema05/Program.cs
+++ Problema05/Program.cs
@@ -6,10 +6,10 @@ do
     //Comando Write incompleto
     Console.WriteLine("Deseja sair? (S/N): ");
     //Comando toupper escrito de maneira incorreta
-    resposta = Console.ReadLine().ToUpper();
+    resposta = (Console.ReadLine() ?? string.Empty).Trim().ToUpper();
     Console.WriteLine(resposta);
 }
-//Condição errada comparador de string não pode ser escrito da maneira como estava escrita, é necessário utilizar o comando equals que compara strings
+//Condição errada comparador de string não pode ser escrito da maneira como estava escrita, podesse utilizar o comando equals que compara strings
 while (!string.Equals(resposta,"S"));
 //Comando escrito de forma incorreta
     Console.WriteLine("Programa encerrado.");
